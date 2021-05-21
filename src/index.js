ec52fe6a224e82006b13f012b2a974bc4db7f1d9
oeleomar <eleomar.dorneles.18@outlook.com>
2021-05-21 13:44:28 -0400

    JS add

--- src/index.js
+++ src/index.js
@@ -1,20 +1,52 @@
-const listBanner = document.querySelector('.list-banners')
+function add_banner(){
+    
+    const name_filme = document.querySelector('#name_film').value
+    const url_filme = document.querySelector('#url_film').value 
 
-const figure = document.createElement('figure')
-figure.classList.add('.wrapper-banner')
+    const verificador = verificadorDeUrl(name_filme, url_filme)
+    if(verificador == true){
+        writeOnScreen(name_filme, url_filme)
+        const limpa_filme = document.querySelector('#name_film').value = ''
+        const limpa_url = document.querySelector('#url_film').value = ''
+    }else if (verificador == false){
+        const limpa_filme = document.querySelector('#name_film').value = ''
+        const limpa_url = document.querySelector('#url_film').value = ''
+    }
+}
 
-const img = document.createElement('img')
-img.src = '/AluraFlix/IMG/banner4.png'
-img.alt = 'Banner do Filme'
-img.classList.add('main-banner')
+function verificadorDeUrl(name_film, url_film){
+    if (name_film = ""|| url_film == ""){
+        window.alert('Verifique os campos')
+        return false
+    } else if (url_film.startsWith() == 'http'){
+        window.alert('Verifique os campos')
+        return false
+    }
+    return true
+}
 
-const title = document.createElement('figcaption')
-title.classList.add('main-caption')
-title.append("Texto do filme")
+function writeOnScreen(name_film, url_film){  
+
+    const listBanner = document.querySelector('.list-banners')
+
+    const figure = document.createElement('figure')
+    figure.classList.add('wrapper-banner')
+
+    const img = document.createElement('img')
+    img.src = `${url_film}`
+    img.alt = 'Banner do Filme'
+    img.classList.add('main-banner')
+
+    const title = document.createElement('figcaption')
+    title.classList.add('main-caption')
+    title.append(name_film)
 
 
-figure.insertAdjacentElement('beforeend', img)
-figure.insertAdjacentElement('beforeend', title)
+    figure.insertAdjacentElement('beforeend', img)
+    figure.insertAdjacentElement('beforeend', title)
+    console.log(figure)
 
 
-listBanner.insertAdjacentElement('beforeend', figure)
+    listBanner.insertAdjacentElement('beforeend', figure)
+
+}
